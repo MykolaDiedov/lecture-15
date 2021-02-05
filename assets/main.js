5d208c0204adcc66767e1ce98ff928f198f0866c
MykolaDiedov <diedov.nn@gmail.com>
2021-02-05 09:47:53 +0200

    second commit

--- assets/main.js
+++ assets/main.js
@@ -1,54 +1,33 @@
-function revers (string) {
+function reverse (string) {
     if(string.length - 1 !== 0 ) {
 
-        return revers(string.slice(1)) + string[0]
+        return reverse(string.slice(1)) + string[0]
     } 
     return string
 }
-console.log(revers('hello guys'))
+console.log(reverse('hello guys'))
 
 
-function searchBinary (arr, finding) {
-    let left = 0;
-    let right = arr.length + 1;
-    const middle = Math.floor((left + right) / 2); // номер среднего елемента массива
+function searchBinary (arr, finding, left, right) {
+   
+    if (left <= right) {
+        
+        let middle = Math.floor((left + right) / 2);
 
-    if (middle === finding) {
+            if (arr[middle] > finding) {
 
-        return true
-    }    // проверяем если искомое равно середине выводим тру
+                return searchBinary(arr, finding, left, middle - 1)
+            }
+            else if (arr[middle] < finding) {
 
-    else if (finding < middle) {
-        left = middle - 1
-    } 
-    else { 
-        right = middle + 1 
+             return searchBinary(arr, finding, middle + 1, right)
+            }
+
+            return true;
+    } else {
+
+        return false ;
     }
-    
-    
-
-    
-    
-    
 }
-
-console.log(searchBinary([1,2,3,4,5,6,7,8,9,10], 7));
-
-/*if(!arr.length) {
-    
-        return false 
-    } else  {
-        
-
-
-        
-        return arr[middle] !== finding ? 
-        searchBinary((arr[middle] > finding) ? 
-        arr.slice(0, middle) : arr.slice(middle + 1), finding) : arr[middle];
-    }*/
-
-/*function revers(string) {
-
-    return string.length - 1 ? revers(string.slice(1)) + string[0] : string;
-};
-console.log(revers('12345'));*/
+let arr = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15]
+console.log(searchBinary(arr, 15, 0, arr.length - 1));
